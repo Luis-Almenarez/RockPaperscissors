6b47f34ac9401039e05f91e8dbc097044954324f
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-09-23 19:39:09 -0500

    Comentando código JS

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,6 +1,8 @@
+// Declaración de variables para llevar el registro de los puntos del jugador y la computadora
 let pointsUser = 0;
 let pointsPC = 0;
 
+// Seleccionar elementos HTML relevantes y asignarlos a variables para su posterior uso
 let instructions = document.querySelector(".instructions");
 let containerPointsUser = document.querySelector(".point__user");
 let containerPointPC = document.querySelector(".point__pc");
@@ -13,46 +15,53 @@ let winMessage = document.querySelector(".win__message");
 let reset = document.querySelector("#reset");
 let buttonsArms = document.querySelectorAll(".arm");
 
+// Agregar un evento de clic a cada botón de arma
 buttonsArms.forEach((button) => {
   button.addEventListener("click", startGame);
 });
 
+// Agregar un evento de clic al botón de reinicio
 reset.addEventListener("click", resetGame);
 
+// Función principal del juego que se ejecuta al hacer clic en un botón de arma
 function startGame(e) {
+  // Generar una elección aleatoria para la computadora (0, 1, o 2)
   let numberPC = Math.floor(Math.random() * 3);
   let selectionUser = e.currentTarget.id;
 
+  // Asignar valores legibles por humanos en lugar de números a la elección de la computadora
   if (numberPC === 0) {
-    numberPC = "rock🪨";
+    numberPC = "Piedra🪨";
   } else if (numberPC === 1) {
-    numberPC = "paper📃";
+    numberPC = "Papel📃";
   } else if (numberPC === 2) {
-    numberPC = "scissors✂️";
+    numberPC = "Tijeras✂️";
   }
 
+  // Determinar el resultado del juego y actualizar la interfaz en consecuencia
   if (
-    (selectionUser === "rock🪨" && numberPC === "scissors✂️") ||
-    (selectionUser === "scissors✂️" && numberPC === "paper📃") ||
-    (selectionUser === "paper📃" && numberPC === "rock🪨")
+    (selectionUser === "Piedra🪨" && numberPC === "Tijeras✂️") ||
+    (selectionUser === "Tijeras✂️" && numberPC === "Papel📃") ||
+    (selectionUser === "Papel📃" && numberPC === "Piedra🪨")
   ) {
-    winUser();
+    winUser(); // El jugador gana
     electionUser.textContent = selectionUser;
     electionPC.textContent = numberPC;
   } else if (
-    (numberPC === "rock🪨" && selectionUser === "scissors✂️") ||
-    (numberPC === "scissors✂️" && selectionUser === "paper📃") ||
-    (numberPC === "paper📃" && selectionUser === "rock🪨")
+    (numberPC === "Piedra🪨" && selectionUser === "Tijeras✂️") ||
+    (numberPC === "Tijeras✂️" && selectionUser === "Papel📃") ||
+    (numberPC === "Papel📃" && selectionUser === "Piedra🪨")
   ) {
-    winPc();
+    winPc(); // La computadora gana
     electionUser.textContent = selectionUser;
     electionPC.textContent = numberPC;
   } else {
-    draw();
+    draw(); // Empate
     electionPC.textContent = numberPC;
     electionUser.textContent = selectionUser;
   }
 
+  // Verificar si alguien ha alcanzado 5 puntos y mostrar un mensaje de resultado
   if (pointsUser === 5) {
     winMessage.classList.remove("losser__message");
     winMessage.classList.add("win__message");
@@ -70,6 +79,7 @@ function startGame(e) {
   }
 }
 
+// Función para manejar la victoria del jugador
 function winUser() {
   pointsUser++;
   containerPointsUser.textContent = pointsUser;
@@ -79,6 +89,7 @@ function winUser() {
   electionUser.textContent = `Haz utilizado `;
 }
 
+// Función para manejar la victoria de la computadora
 function winPc() {
   pointsPC++;
   containerPointPC.textContent = pointsPC;
@@ -87,12 +98,14 @@ function winPc() {
   containerWinPoint.classList.remove("disable");
 }
 
+// Función para manejar empates
 function draw() {
   containerWinPoint.textContent = "¡Empate! 👌";
   message.classList.remove("disable");
   containerWinPoint.classList.remove("disable");
 }
 
+// Función para reiniciar el juego
 function resetGame() {
   location.reload();
 }
